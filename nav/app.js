adc3428fa6b7c92f84c07cf0e12a3270795d1b56
Chocorot <admin@chocorot.net>
2023-02-24 14:35:20 +0800

    update

--- nav/app.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.toggle');
-const menu = document.querySelector('.menu');
-
-toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
