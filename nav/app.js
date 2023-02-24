7d8291a0bf4e76f836bc3a1f73864f279c4e085e
Chocorot <admin@chocorot.net>
2023-02-24 14:16:53 +0800

    update

--- nav/app.js
+++ nav/app.js
@@ -1,23 +1,7 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarNav = document.querySelector('.navbar-nav');
 
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
-
-navSlide();
+navbarToggle.addEventListener('click', function() {
+  navbarToggle.classList.toggle('active');
+  navbarNav.classList.toggle('active');
+});
